2895c929cd589f645f617bf8797508f98a73a981
Tao <werewaswas@live.com>
2018-07-11 05:50:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,27 +15,6 @@ class Student
     # remember each row should be a new instance of the Student class
   end
 
-  # def self.find_by_name(name)
-  #   # find the student in the database given a name
-  #   # return a new instance of the Student class
-  #   sql = <<-SQL
-  #     SELECT
-  #       name
-  #     FROM
-  #       students
-  #     WHERE
-  #       ?
-  #     LIMIT
-  #       1
-  #   SQL
-  #   # row = DB[:conn].execute(sql, name).flatten
-  #   # student = self.new_from_db(row)
-  #   
-  #   DB[:conn].execute(sql,name).map do |row|
-  #     self.new_from_db(row)
-  #   end.first
-  # end
-
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
