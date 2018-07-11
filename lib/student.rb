20e80ff3787a679640cbbe22870ebb9b3d1f6713
Tao <werewaswas@live.com>
2018-07-11 05:54:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,10 @@ class Student
     student
   end
 
+  def self.count_all_students_in_grade_9
+    
+  end
+
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
